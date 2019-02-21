21be1fa1a746eaef89b777fd976c5ff49ea3273f
Keith <kdesimini@bsu.edu>
2019-02-20 22:22:44 -0500

    Add edit and delete functionality.

--- Index.php
+++ Index.php
@@ -1,26 +1,6 @@
-<?php 
-
+<?php
+require './themepart/navbar.php';
 ?>
-<!DOCTYPE HTML>
-<HTML LANG="EN">
-<HEAD>
-    <TITLE>Home</TITLE>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.6/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-
-</HEAD>
-
-<body>
-<div class="btn-group d-flex" role="group" aria-label="Home Buttons">
-    <button type="button" onclick="window.location='/phpTest/Stops.php'"class="btn btn-secondary">Stops</button>
-    <button type="button" onclick="window.location='/phpTest/Users.php'"class="btn btn-secondary">Users</button>
-    <button type="button" onclick="window.location='/phpTest/Entries.php'" class="btn btn-secondary">Entries</button>
-    <button type="button" onclick="window.location='/phpTest/Loops.php'" class="btn btn-secondary">Loops</button>
-</div>
 
 
 
--- Loops.php
+++ Loops.php
@@ -1,130 +1,108 @@
 <?php
- require 'connect.php';
+require 'connect.php';
 
 $loopNames = array();
 $input = "";
 
 if(isset($_POST['SubmitButton'])){
   $input = $_POST['inputText'];
-  
   if($input != '') {
-    
-    postLoop($con, $input); 
+
+    postLoop($con, $input);
   }
-
-   
   header('Location: Loops.php');
-
-
-  
 }
 
 function makeList(&$loopNames, $con) {
- 
+  $sql = sprintf("SELECT * FROM loops");
 
-
-    $sql = sprintf("SELECT * FROM loops");
-
-    if($result = mysqli_query($con,$sql))
-    {
-
-
-      while($row = mysqli_fetch_assoc($result))
-      {
-        array_push($loopNames, $row);
-      }
-
-
-    } else {
-      http_response_code(404);
+  if($result = mysqli_query($con,$sql)) {
+    while($row = mysqli_fetch_assoc($result)) {
+      array_push($loopNames, $row);
     }
+  } else {
+    http_response_code(404);
+  }
 }
 
-
-    function postLoop($con, $input){
-      var_dump($input);
-
-    $sql = sprintf("INSERT INTO `loops`(`loops`) VALUES ( '$input' )");
-
-    if($result = mysqli_query($con,$sql))
-    {
-        // $text = 'Purple Loop';
-
-        // $text = strip_tags($text);
-        // $text = trim($text);
-        // $text = htmlspecialchars($text);
-
-        
-        
-    } else {
-      echo "anything";
-      http_response_code(404);
-    }
-        
-
+function postLoop($con, $input){
+  $sql = sprintf("INSERT INTO `loops`(`loops`) VALUES ( '$input' )");
+  if($result = mysqli_query($con,$sql))
+  {
+    // $text = 'Purple Loop';
+    // $text = strip_tags($text);
+    // $text = trim($text);
+    // $text = htmlspecialchars($text);
+  } else {
+    echo "anything";
+    http_response_code(404);
   }
-
-
-
-
-
+}
 ?>
 
 <html>
 <head>
-  
   <?php
   require './themepart/navbar.php';
   ?>
-
-
-
+<script src="jquery.tabledit.min.js"></script>
 </head>
-
-
 <body>
+  <div align="center">
 
+    <?php
+    makeList($loopNames, $con);
+    ?>
 
-<div align="center">
+    <div class="newLoop">
+      <p>Enter new Loop name here:</p>
+      <form action="" method="post">
+        <?php echo $input; ?>
+        <input name="inputText" input="text" />
+        <input type='submit' name="SubmitButton" class='btn btn-secondary'/>
+      </form>
+    </div>
 
-<?php
-makeList($loopNames, $con);
-?>
+    <table id="editable_table" class="table table-bordered table-striped">
+     <thead>
+      <tr>
+       <th>Loop</th>
+      </tr>
+     </thead>
+     <tbody>
+     <?php foreach ($loopNames as $log): ?>
+          <tr>
+            <td><?php echo $log['loops']; ?></td>
+            <td style="display:none;"><?php echo $log['id']; ?></td>
+            <!-- <td><form action='edit.php?name="<?php echo $log['loops']; ?>"' method="post">
+                  <input type="hidden" name="name" value="<?php echo $log['loops']; ?>">
+                  <input type="submit" name="editButton" value="edit">
+                </form>
+            </td>
+            <td>
+            <form action='delete.php?name="<?php echo $log['loops']; ?>"' method="post">
+                  <input type="hidden" name="name" value="<?php echo $log['loops']; ?>">
+                  <input type="submit" name="editButton" value="delete">
+                </form>
+            </td> -->
 
-<table>
-   <tr>
-    <th>Loops</th>
-  </tr>
-<ul id="ListOfLogs">
-<?php foreach ($loopNames as $log): ?>
- <tr>
-     <td>
-       <?php echo $log['loops']; ?>
-       <?php echo "<button type = 'button' class='btn btn-secondary'> edit</button>"?>
-      </td>
-     
- </tr>
-<?php endforeach; ?>
-</ul>
-</table>
-
-
-
-
-<div class="newLoop">
-<p>Enter new Loop name here:</p>
-
-<form action="" method="post">
-<?php echo $input; ?>
-  <input name="inputText" input="text" />
-  <input type='submit' name="SubmitButton" class='btn btn-secondary'/>
-</form>
-
-</div>
-
-</div>
-
-
+          </tr>
+        <?php endforeach; ?>
+     </tbody>
+    </table>
+  </div>
 </body>
 
+<script>  
+$(document).ready(function(){  
+  $('#editable_table').Tabledit({
+    url: 'actionLoops.php',
+    columns: {
+        identifier: [1, 'id'],
+        editable: [[0,'loop']]
+    }
+});
+ 
+});  
+ </script>
 </html>
