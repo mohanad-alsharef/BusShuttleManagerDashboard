23624a3d1b169a867b608df1426a356a5b0fb1e3
Keith DeSimini <kdesimini@bsu.edu>
2019-03-25 14:09:09 -0400

    Fix Everything

--- Pages/EntryReports.php
+++ Pages/EntryReports.php
@@ -161,6 +161,7 @@
         <div class="col-auto">
           
           <button type="submit" name="HourlyButton" class="btn btn-dark mb-2">Filter By Hour</button>
+          <a href="#" id="xx" class="btn btn-dark mb-2">Export</a>
 
         </div>
         </div>
@@ -210,7 +211,7 @@
 
     <!-- This adds the sql info the hourly display -->
         <?php $time = 12; ?>
-        <tbody class="row_position">
+        <tbody id="tbodyid" class="row_position">
             <?php                
                $counter = 0;
                foreach($loopArray as $loop){ ?>
@@ -260,7 +261,64 @@
 </body>
 
 
+<script>
+    $(document).ready(function () {
 
+function exportTableToCSV($table, filename) {
+
+    var $rows = $table.find('tr:has(td),tr:has(th)'),
+
+        // Temporary delimiter characters unlikely to be typed by keyboard
+        // This is to avoid accidentally splitting the actual contents
+        tmpColDelim = String.fromCharCode(11), // vertical tab character
+        tmpRowDelim = String.fromCharCode(0), // null character
+
+        // actual delimiter characters for CSV format
+        colDelim = '","',
+        rowDelim = '"\r\n"',
+
+        // Grab text from table into CSV formatted string
+        csv = '"' + $rows.map(function (i, row) {
+            var $row = $(row), $cols = $row.find('td,th');
+
+            return $cols.map(function (j, col) {
+                var $col = $(col), text = $col.text();
+
+                return text.replace(/"/g, '""'); // escape double quotes
+
+            }).get().join(tmpColDelim);
+
+        }).get().join(tmpRowDelim)
+            .split(tmpRowDelim).join(rowDelim)
+            .split(tmpColDelim).join(colDelim) + '"',
+
+        
+
+        // Data URI
+        csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+        
+        console.log(csv);
+        
+        if (window.navigator.msSaveBlob) { // IE 10+
+            //alert('IE' + csv);
+            window.navigator.msSaveOrOpenBlob(new Blob([csv], {type: "text/plain;charset=utf-8;"}), "csvname.csv")
+        } 
+        else {
+            $(this).attr({ 'download': filename, 'href': csvData, 'target': '_blank' }); 
+        }
+}
+
+// This must be a hyperlink
+$("#xx").on('click', function (event) {
+    
+    exportTableToCSV.apply(this, [$('#editable_table'), 'export.csv']);
+    
+    // IF CSV, don't do event.preventDefault() or return false
+    // We actually need this to be a typical hyperlink
+});
+
+});
+</script>
 </HTML>
 
 <?php require '../themepart/footer.php'; ?>
