8176745516f674e3390306bd0c24ddd7fb656405
taneia <trreed2@bsu.edu>
2019-03-28 20:40:11 -0400

    Fixing Login Authentication

--- Pages/Register.php
+++ Pages/Register.php
@@ -1,7 +1,7 @@
 <?php
-require_once 'config.php';
+require '../Database/connect.php';
  
-$username = $password = $confirm_password = "";
+$firstname = $lastname = $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
  
 
@@ -51,7 +51,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
 
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        $sql = "INSERT INTO admins (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO admins (firstname, lastname, username, password) VALUES (?, ?, ?, ?)";
          
         if($stmt = mysqli_prepare($con, $sql)){
             mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
