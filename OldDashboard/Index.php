37bd9e7c77affb2ccfdace665e8df50a15d196ff
Keith <kdesimini@bsu.edu>
2019-07-17 14:34:08 -0500

    Revert old dashboard to base directory.

--- OldDashboard/Index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-header("Location: ./Pages/Users.php"); /* Redirect browser */
-exit();
-?>
--- View/index.php
+++ View/index.php
@@ -1,27 +1,29 @@
 <?php
-
+require ('../Model/User.php');
 //include the configuration
 require_once(dirname(__FILE__) . '/../Configuration/config.php');
+
 
 //instance of AccessLayer
 $AccessLayer = new AccessLayer();
 
-$users = Array(3);
+$users = Array(1);
 
 foreach ($users as $user) {
-    $results = $AccessLayer->get_user($user);
+    $results = $AccessLayer->get_all_users_as_User_Objects();
     echo "<h1>Users by $user</h1>";
-
+    //$AccessLayer->update_user(1, "hello", "world");
+    //$AccessLayer->remove_user(1);
 
     if ($results){
         echo "<ul>";
 
-        foreach ($results as $model) {
-           echo "<li>$model->firstname (Database ID: $model->id)</li>";
+        foreach ($results as $user) {
+           echo "<li>$user->firstname (Database ID: $user->id)</li>";
        }
         echo "</ul>";
     } else {
-        echo "sorry, nothing to show here you ass.";
+        echo "Sorry nothing to show here.";
     }
 }
 
